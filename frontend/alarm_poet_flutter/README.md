bfe09150c50793bde8289edb71b2cc60b77a31cb
Harshil Vaghjiani <hazit_90@hotmail.com>
2025-05-10 14:43:21 +0530

    Added basic flutter project

---
+++ frontend/alarm_poet_flutter/README.md
@@ -0,0 +1,3 @@
+# alarm_poet_flutter
+
+A new Flutter project.
---
+++ frontend/alarm_poet_flutter/android/app/src/main/kotlin/com/example/alarm_poet_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.alarm_poet_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
