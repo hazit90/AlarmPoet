b436237622c2f094f3d1fe87f1941e85b8e30a51
Harshil Vaghjiani <hazit_90@hotmail.com>
2025-05-10 18:51:13 +0530

    Add location functionality and update project configurations

--- frontend/alarm_poet_flutter/lib/viewmodel/set_alarm_vm.dart
+++ frontend/alarm_poet_flutter/lib/viewmodel/set_alarm_vm.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
 class SetAlarmVm extends ChangeNotifier {
-  String _locationName = "Current Location";
+  String _locationName = "Fetching location...";
   DateTime _sunriseTime = DateTime.now().subtract(const Duration(hours: 1));
   DateTime _alarmTime = DateTime.now().subtract(const Duration(hours: 1));
+
+  LocationData? _locationData;
 
   String get locationName => _locationName;
 
@@ -11,7 +14,11 @@ class SetAlarmVm extends ChangeNotifier {
 
   String get alarmString => _formatTime(_alarmTime);
 
-  DateTime get alarmTime => _alarmTime; // Added getter for alarmTime
+  DateTime get alarmTime => _alarmTime;
+
+  SetAlarmVm() {
+    _initializeLocation();
+  }
 
   void setOffset(Duration offset) {
     _alarmTime = _sunriseTime.add(offset);
@@ -23,8 +30,44 @@ class SetAlarmVm extends ChangeNotifier {
     notifyListeners();
   }
 
-  Future<void> updateLocationName() async {
-    _locationName = "Current Location";
+  Future<void> _initializeLocation() async {
+    await fetchLocation();
+  }
+
+  Future<void> fetchLocation() async {
+    Location location = Location();
+
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+
+    // Check if location services are enabled
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        _locationName = "Location services disabled";
+        notifyListeners();
+        return; // Exit if the user does not enable location services
+      }
+    }
+
+    // Check for location permissions
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        _locationName = "Location permission denied";
+        notifyListeners();
+        return; // Exit if the user does not grant permissions
+      }
+    }
+
+    // Get the current location
+    _locationData = await location.getLocation();
+
+    // Update the location name (you can use reverse geocoding here if needed)
+    _locationName =
+        "Lat: ${_locationData!.latitude}, Lon: ${_locationData?.longitude}";
     notifyListeners();
   }
 
